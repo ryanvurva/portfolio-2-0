87243f638347f9c6bc7143d8d662661bc950a550
Ryan Vurva <ryanvurva@gmail.com>
2017-04-17 01:26:32 -0400

    made a lot of necessary updates
    still need to fix menu problems and add more functionality.  also need to fix some animations so they act better for the user.  like the hello bubble.  want it to float away on scroll and/orD when sidebar is opened

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Profile from './Profile'
-import Repos from './Repos'
+import Sidebar from './Sidebar'
 import Slide1 from './Slide1'
 
 class App extends Component {
@@ -45,31 +45,9 @@ class App extends Component {
 
   render () {
     return <div>
+      <Sidebar repos={this.state.repos} repoName={this.state.repoName} />
       <Profile name={this.state.name} bio={this.state.bio} location={this.state.location} />
-      <Repos repos={this.state.repos} repoName={this.state.repoName} />
       <Slide1 />
-      <div className='slide-2'>
-        <div className='menu'>
-          <input type='checkbox' id='menu-collapsed' name='menu-collapsed' />
-          <div className='menu-content'>
-            <h2>GitHub Repos</h2>
-            <ul>
-              <li>lorem ipsum</li>
-              <li>dolor sit amet</li>
-              <li>consetetur sadipscing</li>
-              <li>elitr</li>
-              <li>sed diam nonumy</li>
-              <li>eirmod tempor</li>
-              <li>invidunt ut</li>
-              <li>labore et dolore</li>
-            </ul>
-          </div>
-          <div className='menu-switch'>
-            <label className='collapse' htmlFor='menu-collapsed'>«</label>
-            <label className='rise' htmlFor='menu-collapsed'>»</label>
-          </div>
-        </div>
-      </div>
     </div>
   }
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import GitHubLogo from '../images/github.png'
+import FacebookLogo from '../images/facebook.png'
+import TwitterLogo from '../images/twitter.png'
+import LinkedInLogo from '../images/linkedin.png'
+
+class Repos extends Component {
+  render () {
+    return <div className='side-bar'>
+      <div className='menu'>
+        <input type='checkbox' id='menu-collapsed' name='menu-collapsed' />
+        <div className='menu-content'>
+          <h2 className='gitHubTitle'>GitHub Repos</h2>
+          <ul className='gitHub'>
+            <li><a href={this.props.repos[0]}>Memory Game (React)</a></li>
+            <li><a href={this.props.repos[1]}>Tic-Tac-Toe Game</a></li>
+            <li><a href={this.props.repos[2]}>My Original Portfolio Project</a></li>
+            <li><a href={this.props.repos[3]}>An HSL Color Picker (React)</a></li>
+          </ul>
+          <h2 className='socialMediaTitle'>Connect To Me</h2>
+          <ul className='socialMedia'>
+            <li><a href='https://github.com/ryanvurva'><i className='fa fa-github-square' /></a></li>
+            <li><a href='https://www.facebook.com/ryan.vurva'><i className='fa fa-facebook-square' /></a></li>
+            <li><a href='https://twitter.com/theeffinvurv'><i className='fa fa-twitter-square' /></a></li>
+            <li><a href='https://www.linkedin.com/in/ryan-vurva-0993bb13b/'><i className='fa fa-linkedin-square' /></a></li>
+          </ul>
+        </div>
+        <div className='menu-switch'>
+          <label className='collapse' htmlFor='menu-collapsed'>«</label>
+          <label className='rise' htmlFor='menu-collapsed'>»</label>
+        </div>
+      </div>
+    </div>
+
+    /* <div className='repos'>
+      <h2>Check out some of my Repositories on <a href='https://github.com/ryanvurva'>GitHub</a></h2>
+      <div className='repo-menu'>
+        <ul>
+          <input type='checkbox' id='collapse' />
+          <label htmlFor='collapse' />
+          <li><a href={this.props.repos[0]}>Memory Game (React)</a></li>
+          <li><a href={this.props.repos[1]}>Tic-Tac-Toe Game</a></li>
+          <li><a href={this.props.repos[2]}>My Original Portfolio Project</a></li>
+          <li><a href={this.props.repos[3]}>An HSL Color Picker (React)</a></li>
+        </ul>
+      </div>
+    </div> */
+  }
+}
+
+export default Repos
